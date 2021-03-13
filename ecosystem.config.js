bea8a7dae0be6edd1bdb7430f66cce1fc6f2b242
Peter Koeppen <pkoeppen@lenovo.com>
2021-03-13 09:13:54 -0500

    fix: updated pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'muffin-client',
+      exec_mode: 'cluster',
       instances: 1,
       maxMemoryRestart: '512M',
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
+      watch: false,
     },
   ],
 };
