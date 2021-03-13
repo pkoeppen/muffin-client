b24ec9e273e6c459e6413a25178fb6e4ef24f1b5
Peter Koeppen <pkoeppen@lenovo.com>
2021-03-13 10:45:50 -0500

    feat: added open graph things

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,12 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:title', property: 'og:title', content: 'Muffin Quest' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Delicious, homemade muffin delivery.',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
